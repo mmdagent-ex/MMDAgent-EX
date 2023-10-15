8d9ecc0b3d27af6223c5c2c33270347cf5ed7823
Akinobu Lee <ri@nitech.ac.jp>
2023-10-15 20:41:27 +0900

    updated copyright header

--- Library_MMDAgent/src/lib/ContentManagerThreadWeb.cpp
+++ Library_MMDAgent/src/lib/ContentManagerThreadWeb.cpp
@@ -1,43 +1,18 @@
-/* ----------------------------------------------------------------- */
-/*           The Toolkit for Building Voice Interaction Systems      */
-/*           "MMDAgent" developed by MMDAgent Project Team           */
-/*           http://www.mmdagent.jp/                                 */
-/* ----------------------------------------------------------------- */
-/*                                                                   */
-/*  Copyright (c) 2009-2016  Nagoya Institute of Technology          */
-/*                           Department of Computer Science          */
-/*                                                                   */
-/* All rights reserved.                                              */
-/*                                                                   */
-/* Redistribution and use in source and binary forms, with or        */
-/* without modification, are permitted provided that the following   */
-/* conditions are met:                                               */
-/*                                                                   */
-/* - Redistributions of source code must retain the above copyright  */
-/*   notice, this list of conditions and the following disclaimer.   */
-/* - Redistributions in binary form must reproduce the above         */
-/*   copyright notice, this list of conditions and the following     */
-/*   disclaimer in the documentation and/or other materials provided */
-/*   with the distribution.                                          */
-/* - Neither the name of the MMDAgent project team nor the names of  */
-/*   its contributors may be used to endorse or promote products     */
-/*   derived from this software without specific prior written       */
-/*   permission.                                                     */
-/*                                                                   */
-/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            */
-/* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       */
-/* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          */
-/* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE          */
-/* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS */
-/* BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,          */
-/* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   */
-/* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,     */
-/* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON */
-/* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   */
-/* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    */
-/* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           */
-/* POSSIBILITY OF SUCH DAMAGE.                                       */
-/* ----------------------------------------------------------------- */
+/*
+  Copyright 2022-2023  Nagoya Institute of Technology
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 /* headers */
 #define POCO_NO_AUTOMATIC_LIBS
@@ -679,7 +654,7 @@ void ContentManagerThreadWeb::abort()
          }
       }
    }
-   
+
    /* close file downloading socket on another thread */
    if (m_fetchStarted)
       m_files[m_currentId]->abort();
@@ -756,7 +731,7 @@ void ContentManagerThreadWeb::run()
       m_finished = true;
       return;
    }
-   
+
    if (m_kill == true)
       return;
 
@@ -825,7 +800,7 @@ void ContentManagerThreadWeb::run()
    }
    zf->close();
    delete zf;
-   
+
    if (m_fetchContentListOnly) {
       /* do not download, just update time stampcheck */
       KeyValue *prop;
@@ -847,7 +822,7 @@ void ContentManagerThreadWeb::run()
 
    if (m_kill == true)
       return;
-   
+
    /* fetch or renew package description from the url */
    if (m_url[MMDAgent_strlen(m_url) - 1] == '/') {
       MMDAgent_snprintf(buff, MMDAGENT_MAXBUFLEN, "%s%s", m_url, CONTENTMANAGER_PACKAGEFILE);
@@ -859,7 +834,7 @@ void ContentManagerThreadWeb::run()
 
    if (m_kill == true)
       return;
-   
+
    /* extract package description */
    desc = new KeyValue;
    desc->setup();
--- Library_MMDFiles/src/lib/PTree.cpp
+++ Library_MMDFiles/src/lib/PTree.cpp
@@ -1,3 +1,18 @@
+/*
+  Copyright 2022-2023  Nagoya Institute of Technology
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 /* ----------------------------------------------------------------- */
 /*           The Toolkit for Building Voice Interaction Systems      */
 /*           "MMDAgent" developed by MMDAgent Project Team           */
@@ -345,12 +360,12 @@ void PTree::release()
 const char *PTree::firstKey(void **save)
 {
    PNode *n;
- 
+
    n = parseToNext(m_root);
    if (n == NULL) return NULL;
 
    *save = (void **)n;
- 
+
    return n->key;
 }
 
